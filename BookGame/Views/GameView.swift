2dd91941920bd94491540a737b5d5a922d93fa93
Yani <yaniyankov0106@gmail.com>
2024-11-17 23:31:02 +0200

    - added the title and description of the game in the start screen

--- BookGame/Views/GameView.swift
+++ BookGame/Views/GameView.swift
@@ -10,12 +10,29 @@ struct GameView: View {
         NavigationStack(path: $viewModel.path) {
             VStack {
                 if viewModel.path.isEmpty {
+                    Text(viewModel.model?.title ?? "")
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding(20)
+                        .background(Color.accentColor.opacity(0.2))
+                        .cornerRadius(20)
+                    Spacer()
+                    Text(viewModel.model?.description ?? "")
+                        .fontWeight(.medium)
+                    Spacer()
                     Button("Start Game") {
                         guard let startChapter = viewModel.getChapter(forType: .start) else { return }
                         viewModel.path.append(startChapter)
                     }
+                    .padding(20)
+                    .background(Color.accentColor.opacity(0.10))
+                    .cornerRadius(20)
+                    Spacer()
                 }
             }
+            .padding(20)
+            .cornerRadius(20)
             .navigationDestination(for: ChapterModel.self) { chapter in
                 ChapterView(forChapter: chapter)
                 .navigationBarBackButtonHidden(true)
